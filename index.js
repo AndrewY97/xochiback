e66e4064279f2ad5a3f399adbbf13ea7baead876
Andrew Yerovi <andreau@semsamx.com>
2024-08-23 15:24:30 -0600

    server configurado

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 const app = express();
-const cors = require('cors')
 const port = 3000;
 
 const indexRouter = require('./routes/index');
 
 app.use(cors());
 app.use(express.json());
+
+// Configuración para servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
 
 app.get('/health', (req, res) => {
     res.status(200).send('OK');
 });
-
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
